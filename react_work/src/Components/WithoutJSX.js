58fc2470a84ea3b28fa33171af6ff7743c9c8743
Sai Kumar Katakam <kskms2000@gmail.com>
2024-12-24 12:54:34 -0500

    changed

--- react_work/src/Components/WithoutJSX.js
+++ react_work/src/Components/WithoutJSX.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Greet(){
-    // return(
-    //     <div>
-    //         <h1>Hello World</h1>
-    //     </div>
-    // )
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
 
-    return React.createElement('div',null,React.createElement('h1',null,'Hello World1'))
+
+    //for react if we want to return html without JSX then we have to use React.createelement method for that other wise if we just use () then it will directly converts to react and return the HTML code to render.
+    //return React.createElement('div',null,React.createElement('h1',null,'Hello World1'))
 }
 
 export default Greet
